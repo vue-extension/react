88ed8789fca0c737c90fafad81f3a6b0523d9be0
chsword <chsword@126.com>
2020-06-05 00:32:19 +0800

    ````

--- example/src/vext-react/index.ts
+++ example/src/vext-react/index.ts
@@ -1,7 +1,17 @@
 import { Vue, Component as VueComponent, Prop } from "vue-property-decorator";
 import { VueConstructor } from 'vue';
-class Component extends Vue {
-    name = "ccccc"
+function chsword() {
+    console.log("chsword(): 加载.");
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log("chsword", target, propertyKey, descriptor);
+        return class extends Vue {
+            name = 'HCReact';
+        }
+    }
+}
+
+@chsword()
+abstract class Component {
 
 }
 
